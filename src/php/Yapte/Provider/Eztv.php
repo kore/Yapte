230b73cbb869e564792899b5ae08dbc3f1e82d91
Kore Nordmann <mail@kore-nordmann.de>
2013-01-27 16:03:36 +0100

    Added: Provider stub

---
+++ src/php/Yapte/Provider/Eztv.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Yapte
+ *
+ * @version $Revision$
+ */
+
+namespace Yapte\Provider;
+
+use Yapte\Provider;
+
+/**
+ * Provider based on eztv.it
+ *
+ * @version $Revision$
+ */
+class Eztv extends Provider
+{
+}
