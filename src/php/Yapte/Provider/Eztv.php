a1709a0753695dcf908f5e3285373e11ed5357a4
Kore Nordmann <mail@kore-nordmann.de>
2013-02-10 21:39:53 +0100

    Stubbed eztv provider

--- src/php/Yapte/Provider/Eztv.php
+++ src/php/Yapte/Provider/Eztv.php
@@ -8,6 +8,7 @@
 namespace Yapte\Provider;
 
 use Yapte\Provider;
+use Yapte\HttpClient;
 
 /**
  * Provider based on eztv.it
@@ -16,4 +17,52 @@ use Yapte\Provider;
  */
 class Eztv extends Provider
 {
+    /**
+     * HTTP Client
+     *
+     * @var HttpClient
+     */
+    protected $httpClient;
+
+    /**
+     * Construct from dependecies
+     *
+     * @param HttpClient $httpClient
+     * @return void
+     */
+    public function __construct(HttpClient $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * Get show list
+     *
+     * @return Show[]
+     */
+    public function getShowList()
+    {
+        throw new \RuntimeException("@TODO: Implement.");
+    }
+
+    /**
+     * Get epiosode list
+     *
+     * @return Episode[]
+     */
+    public function getEpisodeList(Show $show)
+    {
+        throw new \RuntimeException("@TODO: Implement.");
+    }
+
+    /**
+     * Get torrents for episode
+     *
+     * @param Episode $episode
+     * @return Torrent[]
+     */
+    public function getTorrentList(Episode $episode)
+    {
+        throw new \RuntimeException("@TODO: Implement.");
+    }
 }
