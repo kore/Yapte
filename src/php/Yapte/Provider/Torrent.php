7d5f01fa885474eba27b5c0a7d45348c3f6c92d1
Kore Nordmann <mail@kore-nordmann.de>
2013-02-16 14:47:30 +0100

    Added: Missing struct

---
+++ src/php/Yapte/Provider/Torrent.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Yapte
+ *
+ * @version $Revision$
+ */
+
+namespace Yapte\Provider;
+
+use Yapte\Struct;
+
+/**
+ * Provider base class
+ *
+ * @version $Revision$
+ */
+class Torrent extends Struct
+{
+    /**
+     * Torrent URL
+     *
+     * @var string
+     */
+    public $url;
+}
